b704053add5ef515d8ae0eb0e1e61e9adaf572ad
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-08-08 23:18:04 +0530

    Add pretrained VGG16 convnet with fine-tuning of top layers

---
+++ Keras_cats_dogs_convnet_pretrained_tuning.py
@@ -0,0 +1,90 @@
+#This method allows Data Augmentation but should only be run on GPU
+from keras.applications import VGG16
+#Just reuse the bottom convolutional base of VGG16 model as its more generic, top will be related to specific classes so not used
+conv_base = VGG16(weights='imagenet',include_top=False,input_shape=(150, 150, 3))
+
+
+#Adding a sequential model to top of conv_base
+from keras import models
+from keras import layers
+model = models.Sequential()
+model.add(conv_base)
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+#freeze the conv base. Freezing a layer means preventing their weights from being updated during training
+print('This is the number of trainable weights ''before freezing the conv base:', len(model.trainable_weights))
+conv_base.trainable = False
+print('This is the number of trainable weights '
+'after freezing the conv base:', len(model.trainable_weights))
+
+
+train_dir="C:/Users/ravi_shukla/Desktop/dogs-vs-cats_small/train/"
+validation_dir="C:/Users/ravi_shukla/Desktop/dogs-vs-cats_small/validation/"
+from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(
+rescale=1./255,
+rotation_range=40,
+width_shift_range=0.2,
+height_shift_range=0.2,
+shear_range=0.2,
+zoom_range=0.2,
+horizontal_flip=True,)
+
+#Validation Data Set should not be augmented
+test_datagen = ImageDataGenerator(rescale=1./255)
+#Resize all images to 150x150
+train_generator = train_datagen.flow_from_directory(train_dir,target_size=(150, 150),batch_size=32,class_mode='binary')
+validation_generator = test_datagen.flow_from_directory(
+validation_dir,target_size=(150, 150),batch_size=32,class_mode='binary')
+from keras import optimizers
+model.compile(loss='binary_crossentropy',
+optimizer=optimizers.RMSprop(lr=2e-5),
+metrics=['acc'])
+history = model.fit_generator(
+train_generator,
+steps_per_epoch=100,
+epochs=30,
+validation_data=validation_generator,
+validation_steps=50)
+
+#Freezing all the layers except a specific one
+conv_base.trainable = True
+set_trainable = False
+for layer in conv_base.layers:
+    if layer.name == 'block5_conv1':
+        set_trainable = True
+    if set_trainable:
+        layer.trainable = True
+    else:
+        layer.trainable = False
+
+#Fine Tuning the model
+model.compile(loss='binary_crossentropy',
+optimizer=optimizers.RMSprop(lr=1e-5),
+metrics=['acc'])
+history = model.fit_generator(
+train_generator,
+steps_per_epoch=100,
+epochs=100,
+validation_data=validation_generator,
+validation_steps=50)
+
+#Plotting the results
+import matplotlib.pyplot as plt
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
